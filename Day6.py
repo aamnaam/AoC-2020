d54acb852d4478d20ac018bb7bd35cc06aa7ff11
Mohammed Aamaan <71080707+aamnaam@users.noreply.github.com>
2020-12-06 16:46:41 +0530

    6 December

---
+++ Day6.py
@@ -0,0 +1,37 @@
+def get_some_yes(group_answer):
+    question_ids = [False]*26
+    for x in group_answer:
+        if x != '\n':
+            question_ids[ord(x)-97] = True
+    return question_ids.count(True)
+
+
+def get_all_yes(group_answer):
+    individual_answers = group_answer.split("\n")
+    num_members = len(individual_answers)
+    question_ids = [0]*26
+    num_questions_answered = 0
+    for each_answer in individual_answers:
+        for x in each_answer:             # checking each question number
+            question_ids[ord(x)-97] += 1
+    for x in question_ids:
+        if x == num_members:
+            num_questions_answered += 1
+    return num_questions_answered
+
+
+def main():
+    some_yes_sum = 0
+    all_yes_sum = 0
+    input_file = open("Day6").read()
+    group_inputs = input_file.split("\n\n")
+    for each_input in group_inputs:
+        some_yes_sum += get_some_yes(each_input)
+        all_yes_sum += get_all_yes(each_input)
+    print("Part 1: ", some_yes_sum)
+    print("Part 2: ", all_yes_sum)
+
+
+if __name__ == '__main__':
+    main()
+    
